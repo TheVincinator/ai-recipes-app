10de74d097bc1e00621baa5e3ca520bed21e2ece
Wesley_Zeng <wesleyzeng08@gmail.com>
2025-04-24 19:12:19 -0400

    small changes

--- Backend/db.py
+++ Backend/db.py
@@ -23,9 +23,10 @@ class User(db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
+    # allergies = db.Column(db.String, nullable=True)
     
     # Define relationship - one user can have many ingredients
     ingredients = db.relationship('Ingredient', backref='owner', lazy=True, cascade="all, delete-orphan")
